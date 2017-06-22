c62a306d86df5933ad025bc7f28e15a7d797d90b
joao pedro renan <jota.renan@gmail.com>
2017-06-22 12:02:39 -0300

    pushing everything because the lab is being closed

--- app/app.js
+++ app/app.js
@@ -1,1 +1,6 @@
-const app = angular.module('jQuest',['ngRoute']);
+const app = angular.module('jQuest',['ui.router']);
+
+app.controller('MyQuestionsCtrl', function($rootScope, $location)
+{
+   $rootScope.activetab = $location.path();
+});
