9395042ba93b458f5ead4dccb4db958013404876
joao pedro renan <jota.renan@gmail.com>
2017-06-22 11:18:53 -0300

    fixes routing

--- app/routes.js
+++ app/routes.js
@@ -28,81 +28,81 @@ function config($routeProvider, $locationProvider) {
       templateUrl: 'templates/advanced-search.html',
       controller: 'AdvancedSearchCtrl',
       controllerAs: 'avengers'
-    });
+    })
 
     // Rota para a página Avengers
     .when('/insert-question', {
       templateUrl: 'templates/insert-question.html',
       controller: 'InsertQuestionCtrl',
-    });
+    })
 
     // Rota para a página Avengers
     .when('/edit-question', {
       templateUrl: 'templates/edit-question.html',
       controller: 'EditQuestionCtrl',
-    });
+    })
 
 
     // Rota para a página Avengers
     .when('/question', {
       templateUrl: 'templates/question.html',
       controller: 'QuestionCtrl',
-    });
+    })
 
     // Rota para a página Avengers
     .when('/user-profile', {
       templateUrl: 'templates/user-profile.html',
       controller: 'UserProfileCtrl',
-    });
+    })
 
 
     // Rota para a página Avengers
     .when('/edit-profile', {
       templateUrl: 'templates/edit-profile.html',
       controller: 'EditProfileCtrl',
-    });
+    })
 
     // Rota para a página Avengers
     .when('/my-questions', {
       templateUrl: 'templates/my-questions.html',
       controller: 'MyQuestionsCtrl',
-    });
+    })
 
 
     // Rota para a página Avengers
     .when('/my-discussions', {
       templateUrl: 'templates/my-discussions.html',
       controller: 'MyDiscussionsCtrl',
-    });
+    })
 
     // Rota para a página Avengers
     .when('/my-discussions', {
       templateUrl: 'templates/my-discussions.html',
       controller: 'MyDiscussionsCtrl',
-    });
+    })
 
     // Rota para a página Avengers
     .when('/modules-management', {
       templateUrl: 'templates/modules-management.html',
       controller: 'ModulesManagementCtrl',
-    });
+    })
 
     // Rota para a página Avengers
     .when('/domain-management', {
       templateUrl: 'templates/domain-management.html',
       controller: 'DomainManagementCtrl',
-    });
+    })
 
     // Rota para a página Avengers
     .when('/dissertive-question-correction', {
       templateUrl: 'templates/dissertive-question-correction.html',
       controller: 'DissertiveQuestionsCorrectionCtrl',
-    });
+    })
 
     // Caso não seja nenhum desses,
     // redirecione para a rota '/'
     .otherwise ({ redirectTo: '/' });
-});
+};
 
 angular
   .module('jQuest')
