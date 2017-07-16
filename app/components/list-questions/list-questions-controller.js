836ba654b5c829add1722c6a64ac88c14f0f7880
Joao Pedro Renan <jota.renan@gmail.com>
2017-07-16 16:21:18 -0300

    implements http request to list questions page

--- app/components/list-questions/list-questions-controller.js
+++ app/components/list-questions/list-questions-controller.js
@@ -1,43 +1,13 @@
  angular.module('jQuest')
 
-.controller('ListQuestionsCtrl', function($scope, $log) {
+.controller('ListQuestionsCtrl', function($scope, $log, $http) {
   $scope.quantity = 10;
-  //simulates json response
-  $scope.questions = [
-    {
-      id: 876,
-      heading : 'Cite alguns fatores que favorecem o aumento da dependência do cigarro no Brasil.',
-      isMultipleChoice : false
-    },
-    {
-      id: 123,
-      heading : `Os microtúbulos são elementos constituintes do citoplasma das células. Assinale a alternativa abaixo que descreve CORRETAMENTE a funções desses microtúbulos.`,
-      isMultipleChoice : true,
-      alternatives : [
-        'Concentração muscular e constituição do citoesqueleto.',
-        'Contração muscular e orientação da divisão celular.',
-        'Manutenção da forma da célula e formação do fuso mitótico.',
-        'Formação do fuso mitótico e movimentos ameboídes.',
-        'Deslocamentos do cromossomos e movimentos ameboídes'
-      ],
-      correctIndex : 2,
-    },
-    {
-      id: 124,
-      heading : `Duas cargas de 2.10^-5 e -6.10^-5 C estão no vácuo a 3m de distância uma da outra. Qual a intensidade da força entre elas?`,
-      isMultipleChoice : true,
-      alternatives : [
-        '0,12N',
-        '1,2N',
-        '12N',
-        '120N',
-        '1200N'
-      ],
-      correctIndex : 0,
-      module : 'Elétrica',
-      domain : 'Física',
-    }
-  ]
+  $http ({
+       method: 'GET',
+       url: `http://localhost:8080/JQuestWebApplication/GetQuestionsServlet`
+   })
+    .then( response => $scope.questions = response.data);
+
   $scope.confirmQuestion = (questionId) => {
     const MAX_QUESTIONS = 10;
     // if user isnt logged
@@ -49,7 +19,6 @@
     } else if ( !currentlyAnswered.includes(questionId)) {
       currentlyAnswered.push(questionId);
       localStorage.setItem('answered', JSON.stringify(currentlyAnswered));
-
     }
     const questionToBeChecked = $scope.questions.find(q => q.id == questionId);
     // TODO: change css based on this
