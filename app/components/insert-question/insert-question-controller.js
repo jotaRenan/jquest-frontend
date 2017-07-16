981aafd0f84f29ce6844978670eddc1a45384090
Joao Pedro Renan <jota.renan@gmail.com>
2017-07-16 18:45:09 -0300

    implements Reset New Question function

--- app/components/insert-question/insert-question-controller.js
+++ app/components/insert-question/insert-question-controller.js
@@ -1,12 +1,13 @@
  angular.module('jQuest')
 
-.controller('InsertQuestionCtrl', function($scope) {
+.controller('InsertQuestionCtrl', function($scope, $log) {
   // Skeleton of how the new question object is
   $scope.question = {
     heading : '',
     domainId : undefined,
     selectedModuleId : undefined,
-    isMultipleChoice : true,
+    alternatives : [],
+    idt : 'A',
   }
   // Simulates JSON
   $scope.domainsList = [
@@ -23,6 +24,20 @@
   $scope.modulesList = [{name: '----', id: '',}];
   $scope.selectedModule = $scope.modulesList[0];
 
+  $scope.reset = tipo => {
+    const temp = {
+      heading : '',
+      domainId : undefined,
+      selectedModuleId : undefined,
+      idt : tipo,
+    }
+    if (tipo != 'A') {
+      temp.alternatives = Array(5).fill('');
+    } if (tipo === 'V') {
+      temp.alternatives.fill( {assertive : '', isCorrect : false});
+    }
+    $scope.question = temp;
+  }
   // Populates Modules dropdown after a domain is selected
   // TODO: change values of modulesList after request to backend
   $scope.getModules = (domainId) => {
