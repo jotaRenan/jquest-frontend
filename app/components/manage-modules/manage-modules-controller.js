6e7f253e2c43be5a70c761b4da1aac5bfd87204e
Joao Pedro Renan <jota.renan@gmail.com>
2017-07-17 19:45:34 -0300

    improves http requests support

--- app/components/manage-modules/manage-modules-controller.js
+++ app/components/manage-modules/manage-modules-controller.js
@@ -10,12 +10,22 @@
     })
      .then(response => {
        $scope.domains = response.data;
+       $scope.domains.map(domain => domain.modules.map(module => module.domainId = domain.id));
        $scope.modules = response.data.map(domain => $scope.modules.concat(domain.modules));
        $scope.modules = [].concat(...$scope.modules); // flattens bi-dimensional array
      });
    $scope.deleteModule = () => {
      //Deletes module whose id is registered at $scope.selectedModule;
    };
+   $scope.updateModule = () => {
+     // sweet ES6 destructuring
+     const {id, name, description, domainId} = $scope.selectedModule;
+     $http({
+       method: 'POST',
+       url: `http://localhost:8080/JQuestWebApplication/UpdateModuleServlet`,
+       data: {id, name, description, domainId},
+     });
+   }
    $scope.updateFields = () => {
      $scope.selectedModule = $scope.modules.find(m => m.id == $scope.selectedModuleId);
    };
