308b626c1387ea067ce069465c7d58cf4bf52684
Joao Pedro Renan <jota.renan@gmail.com>
2017-07-17 20:06:45 -0300

    prepares Insert Question for http connection in order to submit qsts.

--- app/components/insert-question/insert-question-controller.js
+++ app/components/insert-question/insert-question-controller.js
@@ -24,7 +24,7 @@
       heading : current.heading,
       domainId : current.domainId,
       selectedModuleId : current.selectedModuleId,
-      difficulty: current.difficulty,
+      dificulty: current.dificulty,
       idt : tipo,
     }
     if (tipo != 'A') {
@@ -39,5 +39,20 @@
   // Populates Modules dropdown after a domain is selected
   $scope.getModules = (domainId) => {
     $scope.modulesList = $scope.domainsList.find( domainInList => domainInList.id === domainId).modules;
+  };
+  $scope.submitQuestion = () => {
+    // Name adaptations in order to match backend entities
+    $scope.question['headline'] = $scope.question['heading'];
+    $scope.question['type'] = $scope.question['idt'];
+    $scope.question['moduleId'] = $scope.question['selectedModuleId'];
+    // Registers user's Id
+    $scope.question['userId'] = JSON.parse(sessionStorage.getItem('userInfo')).id;
+    // Sweet es6 destructuring
+    const payload = {domainId, moduleId, userId, headline, dificulty, type} =  $scope.question;
+    $http({
+      method: 'POST',
+      url: `http://localhost:8080/JQuestWebApplication/CreateQuestionServlet`,
+      data: payload,
+    })
   }
 })
