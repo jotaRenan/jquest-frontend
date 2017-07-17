1f3d1b9efd90161a2e12093c46c9f37da3364d08
Joao Pedro Renan <jota.renan@gmail.com>
2017-07-17 18:48:20 -0300

    starts implementing http requests

--- app/components/shared/main-controller.js
+++ app/components/shared/main-controller.js
@@ -1,11 +1,33 @@
  angular.module('jQuest')
 
-.controller('MainCtrl', function($scope, $log) {
+.controller('MainCtrl', function($scope, $log, $http) {
   // TODO: add verification
   const temp = sessionStorage.getItem('hideLogin');
   $scope.hideLogin = !!temp;
   $scope.close = () => {
     $scope.hideLogin = true;
     sessionStorage.setItem('hideLogin', 'true');
+  }
+
+  $scope.logout = () => {
+    sessionStorage.removeItem('loginFlag');
+  }
+
+  $scope.login = () => {
+    if (sessionStorage.getItem('loginFlag')) {
+      return;
+    }
+    $http ({
+         method: 'POST',
+         url: `http://localhost:8080/JQuestWebApplication/Login`,
+         data: {email: $scope.credential.email, password: $scope.credential.password},
+     })
+      .then(response => {
+        sessionStorage.setItem('userInfo', JSON.stringify(response.data));
+      }, response => {
+        if (response.status === 401) {
+          // TODO: msg erro de login
+        }
+      });
   }
 })
