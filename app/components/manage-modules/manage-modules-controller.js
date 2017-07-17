c49fb7cd98153e9f5b70884d034204d96c5d7740
Joao Pedro Renan <jota.renan@gmail.com>
2017-07-17 00:03:37 -0300

    implements http requests on manage-modules page

--- app/components/manage-modules/manage-modules-controller.js
+++ app/components/manage-modules/manage-modules-controller.js
@@ -1,34 +1,21 @@
  angular.module('jQuest')
- .controller('ManageModulesCtrl', function($scope, $log) {
+ .controller('ManageModulesCtrl', function($scope, $log, $http) {
+   // TODO: change attribute names in order to match json
    $scope.selectedModule = undefined;
    $scope.selectedModuleId = undefined;
-   $scope.updateFields = () => {
-     $scope.selectedModule = $scope.modules.find(m => m.id == $scope.selectedModuleId);
-   }
-   $scope.modules = [
-     {
-       id : 1,
-       name : 'Citologia',
-       domainName : 'Biologia',
-       domainId : 1
-     },
-     {
-       id : 2,
-       name : 'Geopolitica',
-       domainName : 'Geography',
-     }
-   ];
-   $scope.domains = [
-     {
-       id: 1,
-       name : 'Biologia',
-     },
-     {
-       id: 2,
-       name: 'Geografia',
-     }
-   ]
+   $http ({
+        method: 'GET',
+        url: `http://localhost:8080/JQuestWebApplication/GetDomainsFullDataServlet`
+    })
+     .then(response => {
+       $scope.domains = response.data;
+       $scope.modules = response.data.map(domain => $scope.modules.concat(domain.modules));
+       $scope.modules = [].concat(...$scope.modules); // flattens bi-dimensional array
+     });
    $scope.deleteModule = () => {
      //Deletes module whose id is registered at $scope.selectedModule;
+   };
+   $scope.updateFields = () => {
+     $scope.selectedModule = $scope.modules.find(m => m.id == $scope.selectedModuleId);
    };
  })
